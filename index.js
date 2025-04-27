3f8669b1a53f502691aa7437f67aec162d64ee30
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2025-04-27 12:40:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/'  (req, res) => {
 res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/hadees', (req, res) => {
+  res.senFile(path.join(__dirname, 'public', 'hadess.html'));
+});
+
 app.listen(port, () => {
 console.log('here we go port is', port);
 });
