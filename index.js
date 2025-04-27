bfe92c9a63fd573cbfc5a2ad93e57ca1f9678f65
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2025-04-27 12:09:53 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.port || 4300;
+
+app.get('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/'  (req, res) => {
+res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+console.log('here we go port is', port);
+});
